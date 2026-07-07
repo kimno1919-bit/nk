0fe46bafd7c75aa755c6deeb279859165bb4956f
LearnHub User <test@example.com>
2026-07-07 15:33:36 +0900

    chore: Google Tag Manager (GTM) 스크립트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Noto_Serif_KR } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
@@ -59,7 +60,26 @@ export default function RootLayout({
       lang="ko"
       className={`${notoSerifKr.variable} h-full antialiased`}
     >
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TRNS6FPT');
+          `}
+        </Script>
+      </head>
       <body className="min-h-full flex flex-col font-sans bg-paper-cream text-ink">
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TRNS6FPT"
+            height="0" 
+            width="0" 
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Header />
         <main className="flex-1 flex flex-col">
           {children}
